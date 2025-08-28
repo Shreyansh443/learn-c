025e83a3556a995c8bb5132908b879b335e7a076
Shreyansh443 <sanctuarycase22@gmail.com>
2025-08-28 12:46:57 +0530

    Learn-c.05 convert temperature from celcius to fahrenheit.cpp

--- Learn-c.05 convert temperature from celcius to fahrenheit.cpp
+++ Learn-c.05 convert temperature from celcius to fahrenheit.cpp
@@ -1,9 +1,10 @@
 // A program to convert temperature from celcius to fahrenheit.
 #include <stdio.h>
 int main(){
-	float cel , fah;
+	float cel;
 	printf("Enter the temperature in Celius");
 	scanf("%f", &cel);
 	printf("The Temperature in fahrenheit is - %f", ((9.0/5.0)*cel)+32);
 	return 0;
 }
+
