0332b7d86a33192047373ff01633fc011b1684e3
Shreyansh443 <sanctuarycase22@gmail.com>
2025-09-02 22:48:16 +0530

    Add files via upload

---
+++ Learn-c.26 To priint numbers from 1 to n.cpp
@@ -0,0 +1,19 @@
+// A program to print numbers from 1 to n
+#include <stdio.h>
+int main(){
+	int n;
+	printf("Enter a number : ");
+	scanf("%d", &n);
+	printf("Numbers form 1 to %d are : \n", n);
+	if (n<=0){
+		printf("Please Enter a positive number");
+	}
+	else{
+		for (int i=1; i<=n; i++){
+			printf("%d\n", &i);
+		}
+	}
+
+	
+	return 0;
+}
