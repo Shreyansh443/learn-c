e6d4368ea91eb1714afd843c97a18a0d549fe9e1
Shreyansh443 <sanctuarycase22@gmail.com>
2025-11-14 18:29:14 +0530

    Day 6

---
+++ learn-13.cpp
@@ -0,0 +1,20 @@
+//A program to check for a leap year
+#include <stdio.h>
+int main(){
+    int y;
+    printf("Enter the Year\n");
+    scanf("%d", &y);
+    if ((y % 4) == 0){
+        if ((y % 100) == 0){
+            if ((y % 400) == 0){
+                printf("%d is a leap year", y);
+            }else {
+                printf("%d is not a leap Year", y);
+            }
+        }else {
+            printf("%d is a leap Year", y);
+        }
+    }else {
+        printf("%d is not a leap Year", y);
+    }
+}
